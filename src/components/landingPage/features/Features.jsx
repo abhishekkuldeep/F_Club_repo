e6db474646f3e2c5909f7fcd30e8d18bcaf0b375
Abhishek Kuldeep <abhishekkuldeep236@gmail.com>
2025-02-22 19:56:29 +0530

    Completed the landing page ui

--- src/components/landingPage/features/Features.jsx
+++ src/components/landingPage/features/Features.jsx
@@ -3,33 +3,39 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './Features.module.css'
+import '../../../index.css'
 
 function Features() {
   const data = [
     {
       title: `Global Networking`,
-      img: `./src/assets/images/global.jpeg`,
+      img: `./src/assets/images/globalimg.png`,
       para: `Dive into thriving communities tailored to your interests. Share ideas, seek advice, and spark innovation by connecting with like-minded individuals.`,
+      icon: `./src/assets/images/green_icon.png`
     },
     {
       title: `Seamless Applications`,
-      img: `./src/assets/images/seamless.jpeg`,
+      img: `./src/assets/images/seamless.png`,
       para: `Dive into thriving communities tailored to your interests. Share ideas, seek advice, and spark innovation by connecting with like-minded individuals.`,
+      icon: `./src/assets/images/yellow_icon.png`
     },
     {
       title: `Industry Highlights`,
-      img: `./src/assets/images/industry.jpeg`,
+      img: `./src/assets/images/industry.png`,
       para: `Dive into thriving communities tailored to your interests. Share ideas, seek advice, and spark innovation by connecting with like-minded individuals.`,
+      icon: `./src/assets/images/red_icon.png`
     },
     {
       title: `Startup Showcase`,
-      img: `./src/assets/images/startup.jpeg`,
+      img: `./src/assets/images/startup.png`,
       para: `Dive into thriving communities tailored to your interests. Share ideas, seek advice, and spark innovation by connecting with like-minded individuals.`,
+      icon: `./src/assets/images/dblue_icon.png`
     },
     {
       title: `Global Networking`,
       img: `./src/assets/images/globalCard.png`,
       para: `Dive into thriving communities tailored to your interests. Share ideas, seek advice, and spark innovation by connecting with like-minded individuals.`,
+      icon: `./src/assets/images/green_icon.png`
     }  
   ]
 
@@ -53,15 +59,39 @@ function Features() {
 
         { data.map( (d)=>(
         
-          <div key={d.title} className={style.cards}>
-            <div className={style.left}>
-              <h1 className={style.title}>{d.title}</h1>
-              <p className={style.para}>{d.para}</p>
+          <div key={d.title}>
+            <div className={style.body}>
+              <div className={style.left}>
+                <h1 className={style.title}>{d.title}</h1>
+                <p className={style.para}>{d.para}</p>
+                
+                <div className={style.barBox}>
+                  <div className={style.bars}>
+                    <img src={d.icon} alt="" />
+                    <p>Welcome to Founders Club</p>
 
-            </div>
+                  </div>
+                  <div className={style.bars}>
+                    <img src={d.icon} alt="" />
+                    <p>Welcome to Founders Club</p>
 
-            <div className={style.right}>
-              <img src={d.img} alt="" />
+                  </div>
+                  <div className={style.bars}>
+                    <img src={d.icon} alt="" />
+                    <p>Welcome to Founders Club</p>
+
+                  </div>
+                </div>
+
+
+                
+
+              </div>
+
+              <div className={style.right}>
+                <img src={d.img} alt="" />
+              </div>
+
             </div>
 
           </div>
@@ -70,6 +100,7 @@ function Features() {
 
         }
         </Slider>
+
 
     </div>
     </>
